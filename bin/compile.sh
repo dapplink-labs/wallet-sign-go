5ee14877a388cbb70673876af8860269d3ca5492
leo.shi <554601845@qq.com>
2024-11-09 16:37:06 +0800

    feature: update proto

--- bin/compile.sh
+++ bin/compile.sh
@@ -12,17 +12,21 @@ function exit_if() {
     fi
 }
 
-echo $GOPATH;
+echo $GOPATH
 
-if [ ! -f $GOPATH/bin/protoc-gen-go ]
+# 检查 protoc-gen-go 是否安装
+if [ ! -f "$(go env GOPATH)/bin/protoc-gen-go" ]
 then
-    echo 'No plugin for golang installed, skip the go installation' >&2
-    echo 'try go get github.com/golang/protobuf/protoc-gen-go' >&2
+    echo 'Protocol Buffers plugin for Go is not installed.' >&2
+    echo 'Please install it using:' >&2
+    echo 'go install google.golang.org/protobuf/cmd/protoc-gen-go@latest' >&2
+    echo 'go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest' >&2
+    exit 1
 else
     echo Compiling go interfaces...
-    export GO_PATH=$GOPATH
-    export GOBIN=$GOPATH/bin
-    export PATH=$PATH:$GOPATH/bin
+    export GO_PATH=$(go env GOPATH)
+    export GOBIN=$GO_PATH/bin
+    export PATH=$PATH:$GO_PATH/bin
 
     protoc -I ./ --go_out=./ --go-grpc_out=require_unimplemented_servers=false:. protobuf/*.proto
 
