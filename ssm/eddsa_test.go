484bbfc94f4dc1c7f67fd591ca1d356ef3f039fd
a-little-change <xiarj186@gmail.com>
2024-12-15 16:54:22 +0800

    verify signature test

--- ssm/eddsa_test.go
+++ ssm/eddsa_test.go
@@ -59,6 +59,6 @@ func TestSignEdDSAMessageV2(t *testing.T) {
 func TestVerifyEdDSASign(t *testing.T) {
 	signature := "9594742341865c897b066714f10bc90f4c2afebba986c3fef500d9d69ea8eb6df19a19160c210dcf65becbe68c1885820915565f3a84277efc34027b73c89608"
 	pubKey := "39f523de37c1218d28ca467a6e0ea0aa0a603064ab402983829513a0feca0039"
-	ok := VerifyECDSASign(pubKey, common.Hash{}.String(), signature)
+	ok := VerifyEdDSASign(pubKey, common.Hash{}.String(), signature)
 	fmt.Println("ok=", ok)
 }
