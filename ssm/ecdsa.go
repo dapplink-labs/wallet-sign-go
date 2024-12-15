a55b882d17edcf7d8bebfba50217500f08294e1f
a-little-change <68430480+a-little-change@users.noreply.github.com>
2024-12-15 19:25:57 +0800

    verify signature test (#6)

--- ssm/ecdsa.go
+++ ssm/ecdsa.go
@@ -43,10 +43,28 @@ func SignECDSAMessage(privKey string, txMsg string) (string, error) {
 	return hex.EncodeToString(signatureByte), nil
 }
 
-func VerifyECDSASign(pubKey, msgHash, sig string) bool {
-	publicKey, _ := hex.DecodeString(pubKey)
-	messageHash, _ := hex.DecodeString(msgHash)
-	signature, _ := hex.DecodeString(sig)
-	fmt.Println(len(publicKey), len(messageHash), len(signature))
-	return crypto.VerifySignature(publicKey, messageHash, signature)
+func VerifyEcdsaSignature(publicKey, txHash, signature string) (bool, error) {
+	// Convert public key from hexadecimal to bytes
+	pubKeyBytes, err := hex.DecodeString(publicKey)
+	if err != nil {
+		log.Error("Error converting public key to bytes", err)
+		return false, err
+	}
+
+	// Convert transaction string from hexadecimal to bytes
+	txHashBytes, err := hex.DecodeString(txHash)
+	if err != nil {
+		log.Error("Error converting transaction hash to bytes", err)
+		return false, err
+	}
+
+	// Convert signature from hexadecimal to bytes
+	sigBytes, err := hex.DecodeString(signature)
+	if err != nil {
+		log.Error("Error converting signature to bytes", err)
+		return false, err
+	}
+
+	// Verify the transaction signature using the public key
+	return crypto.VerifySignature(pubKeyBytes, txHashBytes, sigBytes[:64]), nil
 }
--- ssm/ecdsa_test.go
+++ ssm/ecdsa_test.go
@@ -12,9 +12,13 @@ func TestCreateECDSAKeyPair(t *testing.T) {
 	fmt.Println("cpubKey=", cpubKey)
 }
 
+// privKey = fb26155c1ff94bb97692793d1197d9c6c8091f25f8c8ac703f92695d32c5194b
+// pubKey = 048846b3ce4376e8d58c83c1c6420a784caa675d7f26c496f499585d09891af8fc9167a4b658b57b28211783cdee651caa8b5341b753fa39c995317670123f12d8
+// cpubKey = 028846b3ce4376e8d58c83c1c6420a784caa675d7f26c496f499585d09891af8fc
+
 func TestSignMessage(t *testing.T) {
 	// 0x35096AD62E57e86032a3Bb35aDaCF2240d55421D
-	privKey := "fe13c8e55444107c32f50cca04965f9772fe4fd720ffedd30b347e541fe7a97c"
+	privKey := "fb26155c1ff94bb97692793d1197d9c6c8091f25f8c8ac703f92695d32c5194b"
 	message := "0x3e4f9a460233ec33862da1ac3dabf5b32db01400fba166cdec40ad6dc735b4ab"
 	signature, err := SignECDSAMessage(privKey, message)
 	if err != nil {
@@ -23,11 +27,17 @@ func TestSignMessage(t *testing.T) {
 	fmt.Println("Signature: ", signature)
 }
 
-func TestVerifySign(t *testing.T) {
-	msgHash := "0000000000000000000000000000000000000000000000000000000000000000"
-	// 04fd13e3e6895a2d9d38821e47f60f6c701da7bf243cc1f0a21a15ff8b09322ceda16bbc6d07df0e408dc19ffb756a75bf0c7cbf6836ef89594e0d97cbfeff319c
-	publicKey := "fd13e3e6895a2d9d38821e47f60f6c701da7bf243cc1f0a21a15ff8b09322ceda16bbc6d07df0e408dc19ffb756a75bf0c7cbf6836ef89594e0d97cbfeff319c"
-	signature := "9e922e8deea041d06461b1b1d228593d6ce56427cd5df6b10d177bc2dbf138117bc5543bdc44790f8734f8171903fd324547bad54c403d5f7f7652af115571be01"
-	ok := VerifyEdDSASign(publicKey, msgHash, signature)
-	fmt.Println("ok==", ok)
+func TestVerifyEcdsaSignature(t *testing.T) {
+	CompressedPubKey := "028846b3ce4376e8d58c83c1c6420a784caa675d7f26c496f499585d09891af8fc"
+	txHash := "3e4f9a460233ec33862da1ac3dabf5b32db01400fba166cdec40ad6dc735b4ab"
+	signature := "f8c9ab615ffd81f74d9db8765e25ce260ba3b4da1c6af2a52dedc697dcff833b6cfe576a1b6b7106a6880d8057639d4b87a67001c69594df29d928d6048912f900"
+
+	isValid, err := VerifyEcdsaSignature(CompressedPubKey, txHash, signature)
+	if err != nil {
+		t.Error("Failed to verify signature:", err)
+	}
+
+	if !isValid {
+		t.Error("Signature is invalid")
+	}
 }
